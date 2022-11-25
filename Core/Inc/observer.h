ab18811f4c46a214ef19fba7df6085f013d22234
Cizzorz3 <mohamedaadelezz3@gmail.com>
2022-11-25 14:37:00 +0200

    last

--- Core/Inc/observer.h
+++ Core/Inc/observer.h
@@ -12,8 +12,11 @@
 
 #define MAX_NUM_OF_SUBSCRIBERS 4
 
+//subscriber
+//observer handle
 typedef struct
 {
+	//array of structures
 	void (*subject_ptr) (void);
 	uint8_t state ;
 }subjects_t;
@@ -27,8 +30,7 @@ typedef struct
 void observer_attach(observers_t *observer , subjects_t *subject );
 void observer_detach(observers_t *observer , subjects_t *subject );
 void notify_observer(observers_t *observer , subjects_t *subject );
-uint8_t get_subjet_state(subjects_t * subject);
-void set_subjet_state(subjects_t * subject);
-void update_observers(observers_t *observer);
+uint8_t get_subject_state(subjects_t * subject);
+void set_subject_state(subjects_t * subject);
 
 #endif /* INC_OBSERVER_H_ */
